e3a9c365c66f0c2cb501d8c9500b4c1abd52a217
Lucas Emanoel <lucastheldl@gmail.com>
2026-04-02 00:03:01 -0300

    chore: added-base-api

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import Fastify from "fastify";
+import rateLimit from "@fastify/rate-limit";
+import { formController } from "./form/infra/form.controller";
+
+const app = Fastify({
+  logger: true,
+});
+
+async function buildServer() {
+  // Register rate limit plugin
+  await app.register(rateLimit, {
+    max: 10, // max requests
+    timeWindow: "1 minute",
+  });
+
+  //routes
+  await app.register(formController, { prefix: "/api" });
+
+  return app;
+}
+
+buildServer().then((server) => {
+  server
+    .listen({ port: 3000, host: "0.0.0.0" })
+    .then(() => {
+      console.log("Server running on http://localhost:3000");
+    })
+    .catch((err) => {
+      server.log.error(err);
+      process.exit(1);
+    });
+});
---
+++ src/form/infra/form.controller.ts
@@ -0,0 +1,42 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+
+type FormBody = {
+  name: string;
+  email: string;
+  date: string;
+  message: string;
+};
+
+export async function formController(app: FastifyInstance) {
+  app.post(
+    "/reservation",
+    {
+      config: {
+        rateLimit: {
+          max: 5,
+          timeWindow: "1 minute",
+        },
+      },
+    },
+    async (
+      request: FastifyRequest<{ Body: FormBody }>,
+      reply: FastifyReply,
+    ) => {
+      const { name, email, date, message } = request.body;
+
+      // basic validation
+      if (!name || !email || !date || !message) {
+        return reply.status(400).send({
+          error: "Missing fields",
+        });
+      }
+
+      // simulate saving / processing
+      console.log("New reservation:", request.body);
+
+      return reply.send({
+        message: "Reservation received ✅",
+      });
+    },
+  );
+}
